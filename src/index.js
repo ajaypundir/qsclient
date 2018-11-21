43217c1587b9ff3de604de9d92ce793c3d623e25
Ajay Pundir <apundir@eagleinvsys.com>
2018-11-21 17:20:25 +0530

    commenting setContext

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo'
-import { setContext } from 'apollo-link-context'
+//import { setContext } from 'apollo-link-context'
 import gql from 'graphql-tag'
 
 import App from './App'
