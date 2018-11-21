825c9a1d7423eec051436b52a2c860ef427a04bd
Ajay Pundir <apundir@eagleinvsys.com>
2018-11-21 16:02:55 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { graphql } from 'react-apollo'
+import { gql } from 'apollo-boost'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const POPULAR_REPOSITORIES_LIST = gql`
+{
+  allUsers(offset:0, limit:10) {
+    id,
+    name,
+    age,
+    address,
+    createdAt
   }
 }
+`
 
-export default App;
+const App = graphql(POPULAR_REPOSITORIES_LIST)(props =>
+    <ul>
+        {props.data.loading ? '' : props.data.allUsers.map((row, i) =>
+            <li key={row.id + '-' + row.name}>
+                {row.id} / {row.name} / {row.age} / {row.address}
+
+            </li>
+        )}
+    </ul>
+)
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo'
+import { setContext } from 'apollo-link-context'
+import gql from 'graphql-tag'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const httpLink = new HttpLink({ uri: 'http://10.220.4.139:19090/api/' })
+
+/*const authLink = setContext((_, { headers }) => {
+    const token = '293d69f054db52a770dfd25a2eee998e8ad06989'
+
+    return {
+        headers: {
+            ...headers,
+            authorization: `Bearer ${token}`
+        }
+    }
+})*/
+
+//const link = authLink.concat(httpLink)
+
+const link = httpLink
+
+const client = new ApolloClient({
+    link: link,
+    cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+)
+
+
+const POPULAR_REPOSITORIES_LIST = gql`
+{
+  allUsers(offset:0, limit:10) {
+    id,
+    name,
+    age,
+    address,
+    createdAt
+  }
+}
+`
+
+client.query({ query: POPULAR_REPOSITORIES_LIST }).then(console.log)
+
